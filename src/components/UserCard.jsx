a23814b162bbe9efc7137742d062db243abd383e
Argha-code <arghapanda19@gmail.com>
2025-10-18 16:45:25 +0530

    css

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -23,7 +23,7 @@ const dispatch = useDispatch()
   return (
    <div 
   data-theme="valentine"
-  className="card bg-pink-200 w-80 shadow-lg text-primary-content border-2 border-pink-200 rounded-2xl">
+  className="card bg-white w-80 shadow-lg text-primary-content border-2 border-pink-200 rounded-2xl">
   <figure>
     <img
       src={user.photoUrl}
