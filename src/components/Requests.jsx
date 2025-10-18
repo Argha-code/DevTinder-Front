e3920c6fdbf41900c0285265b9fbb789adbe11e6
Argha-code <arghapanda19@gmail.com>
2025-10-18 13:11:42 +0530

    request filter

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -45,7 +45,9 @@ const Requests = () => {
     <div className=" text-center my-10">
         <h1 className="text-bold text-white text-3xl">Connection Requests</h1>
 
-        {requests?.map((request) => {
+        {requests
+        .filter((request) = request?.fromUserId)
+          .map((request) => {
           const {_id, firstName, lastName, photoUrl, age,gender, about} = request.fromUserId  // extract all and all the things comes from my connection and getting the data request.fromUserId
 
 
